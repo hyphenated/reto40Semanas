9bc2db44a8f020d3dca02daeeda4c2ee7ce4bea5
Jonathan Ortega <jortega0389@gmail.com>
2015-03-30 16:12:44 -0500

    adding some settings

---
+++ tasks/config/sass.js
@@ -0,0 +1,28 @@
+/**
+ * Compiles SASS files into CSS.
+ *
+ * ---------------------------------------------------------------
+ *
+ * Only the `assets/styles/main.scss` is compiled.
+ * This allows you to control the ordering yourself, i.e. import your
+ * dependencies, mixins, variables, resets, etc. before other stylesheets)
+ *
+ * For usage docs see:
+ *      https://github.com/sindresorhus/grunt-sass
+ */
+module.exports = function(grunt) {
+
+    grunt.config.set('sass', {
+        dev: {
+            files: [{
+                expand: true,
+                cwd: 'assets/styles/',
+                src: ['main.scss'],
+                dest: '.tmp/public/styles/',
+                ext: '.css'
+            }]
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+};
