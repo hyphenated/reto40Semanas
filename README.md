3c81746c8025b95fe18624758c5021ef6a76ce45
Jonathan Ortega <jortega0389@gmail.com>
2015-03-30 15:12:01 -0500

    adding sails initial set up

---
+++ README.md
@@ -0,0 +1,5 @@
+#Reto 40 Semanas
+
+App para el tracking del reto de las 40 semanas entre David Camargo y Osvaldo Galvez
+
+Hecho con amor usando Sails.js + MongoDB y Angular.js
---
+++ api/controllers/Weekly_entriesController.js
@@ -0,0 +1,11 @@
+/**
+ * Weekly_entriesController
+ *
+ * @description :: Server-side logic for managing weekly_entries
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
